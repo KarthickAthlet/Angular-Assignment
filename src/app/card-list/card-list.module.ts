31014d70160babaf82234697d3e707bd4bc4484b
DESKTOP-FQBJS4T\Hacky_Shaggy <karthick.psgtec12@gmail.com>
2020-03-08 18:19:20 +0530

    commit (2): * Created components: 	- Core/layouts 	- Filter/sort-filter 	- Filter/search-filter 	- card-list/cards

--- src/app/card-list/card-list.module.ts
+++ src/app/card-list/card-list.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CardListRoutingModule } from './card-list-routing.module';
+import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CardsComponent],
   imports: [
     CommonModule,
     CardListRoutingModule
--- src/app/filter/filter.module.ts
+++ src/app/filter/filter.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FilterRoutingModule } from './filter-routing.module';
+import { SortFilterComponent } from './sort-filter/sort-filter.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SortFilterComponent, SearchFilterComponent],
   imports: [
     CommonModule,
     FilterRoutingModule
