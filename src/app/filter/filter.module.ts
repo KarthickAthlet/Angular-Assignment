5dc76907c370f895e3bb3ca46d013161b8cad577
DESKTOP-FQBJS4T\Hacky_Shaggy <karthick.psgtec12@gmail.com>
2020-03-11 01:01:23 +0530

    commit (13): - Updated Checkbox-filter with dynamic generation of components - Created Mock Json Structure for the filter - Updated Package.json file with port and auto-server start

--- src/app/filter/filter.module.ts
+++ src/app/filter/filter.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FilterRoutingModule } from './filter-routing.module';
+import { SharedModule } from './../shared/shared.module';
 import { SortFilterComponent } from './sort-filter/sort-filter.component';
 import { SearchFilterComponent } from './search-filter/search-filter.component';
 import { CheckboxFilterComponent } from './checkbox-filter/checkbox-filter.component';
@@ -13,7 +14,8 @@ import { CheckboxFilterComponent } from './checkbox-filter/checkbox-filter.compo
   imports: [
     CommonModule,
     FormsModule,
-    FilterRoutingModule
+    FilterRoutingModule,
+    SharedModule
   ],
   exports: [
     SortFilterComponent,
